85ead4e7cbf291822189d6d40302199b52d2f795
alessanderviana <alessander.viana@hotmail.com>
2019-05-29 11:02:00 -0300

    Changed to install Ansible

--- gcp/startup-script.sh
+++ gcp/startup-script.sh
@@ -4,31 +4,39 @@
 locale-gen pt_BR.UTF-8 && \
 rm -f /etc/localtime && sudo ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
-# Docker
-curl -fsSL https://get.docker.com | sh
-usermod -aG docker ubuntu
+# Ansible
+apt-get update
+apt-get install software-properties-common
 
-# Kubernetes
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | tee -a /etc/apt/sources.list
-apt-get update -q && apt-get install -y kubelet kubeadm kubectl
+apt-add-repository -y ppa:ansible/ansible
 
-# Adjust Docker driver (change to systemd)
-cat > /etc/docker/daemon.json <<EOF
-{
- "exec-opts": ["native.cgroupdriver=systemd"],
- "log-driver": "json-file",
- "log-opts": {
-   "max-size": "100m"
- },
- "storage-driver": "overlay2",
- "storage-opts": [
-   "overlay2.override_kernel_check=true"
- ]
-}
-EOF
-mkdir -p /etc/systemd/system/docker.service.d
-systemctl daemon-reload && systemctl restart docker
+apt-get update
+apt-get install ansible
 
-# Needed kubernetes images
-kubeadm config images pull
+# Clone the REPO
+git clone https://github.com/alessanderviana/kubernetes-cluster-in-the-cloud.git
+
+# # Kubernetes
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+# echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' | tee -a /etc/apt/sources.list
+# apt-get update -q && apt-get install -y kubelet kubeadm kubectl
+#
+# # Adjust Docker driver (change to systemd)
+# cat > /etc/docker/daemon.json <<EOF
+# {
+#  "exec-opts": ["native.cgroupdriver=systemd"],
+#  "log-driver": "json-file",
+#  "log-opts": {
+#    "max-size": "100m"
+#  },
+#  "storage-driver": "overlay2",
+#  "storage-opts": [
+#    "overlay2.override_kernel_check=true"
+#  ]
+# }
+# EOF
+# mkdir -p /etc/systemd/system/docker.service.d
+# systemctl daemon-reload && systemctl restart docker
+#
+# # Needed kubernetes images
+# kubeadm config images pull
