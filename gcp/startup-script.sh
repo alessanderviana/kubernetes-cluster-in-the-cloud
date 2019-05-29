424a5e5065c87a3b2605d4e8c36116b071f23c48
alessanderviana <alessander.viana@hotmail.com>
2019-05-29 17:31:16 -0300

    Removed some trash

--- gcp/startup-script.sh
+++ gcp/startup-script.sh
@@ -18,23 +18,3 @@ cd /home/ubuntu && sudo -u ubuntu git clone https://github.com/alessanderviana/k
 
 # Run the playbook
 ansible-playbook /home/ubuntu/kubernetes-cluster-in-the-cloud/ansible/node-install-software.yml
-
-# # Adjust Docker driver (change to systemd)
-# cat > /etc/docker/daemon.json <<EOF
-# {
-#  "exec-opts": ["native.cgroupdriver=systemd"],
-#  "log-driver": "json-file",
-#  "log-opts": {
-#    "max-size": "100m"
-#  },
-#  "storage-driver": "overlay2",
-#  "storage-opts": [
-#    "overlay2.override_kernel_check=true"
-#  ]
-# }
-# EOF
-# mkdir -p /etc/systemd/system/docker.service.d
-# systemctl daemon-reload && systemctl restart docker
-#
-# # Needed kubernetes images
-# kubeadm config images pull
