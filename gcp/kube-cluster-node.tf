8213abcc0d249a7eb349092ac87ab85e17de2b8b
alessanderviana <alessander.viana@hotmail.com>
2019-05-29 11:01:36 -0300

    Changed the count and machine type

--- gcp/kube-cluster-node.tf
+++ gcp/kube-cluster-node.tf
@@ -1,10 +1,9 @@
 
-// Linux instance - Ubuntu 16.04
 resource "google_compute_instance" "kube-cluster-node" {
- count = 2
+ count = 1
  name         = "kube-cluster-node-${count.index + 1}"
- machine_type = "n1-standard-2"  # 7.5 GB RAM
- # machine_type = "n1-standard-1"  # 3.75 GB RAM
+ # machine_type = "n1-standard-2"  # 7.5 GB RAM
+ machine_type = "n1-standard-1"  # 3.75 GB RAM
  zone         = "${var.region}-b"
  allow_stopping_for_update = true
 
@@ -12,7 +11,7 @@ resource "google_compute_instance" "kube-cluster-node" {
 
  boot_disk {
    initialize_params {
-     image = "ubuntu-1604-xenial-v20190306"
+     image = "ubuntu-1804-bionic-v20190514"
      size = 20
    }
  }
@@ -31,12 +30,5 @@ resource "google_compute_instance" "kube-cluster-node" {
  }
 
 metadata_startup_script = "${file("startup-script.sh")}"
-
-# sudo kubeadm init
-# kubectl get pods -n kube-system
-# kubeadm token create --print-join-command
-
-# GutHub token
-# 706272c8197b402d6e1d270a6be2ea08b1e9a1fd
 
 }
