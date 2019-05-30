7512f0e9c383bffca0c1600041ed31c5b62b4c32
alessanderviana <alessander.viana@hotmail.com>
2019-05-30 10:56:26 -0300

    Removed the git clone

--- gcp/startup-script.sh
+++ gcp/startup-script.sh
@@ -4,9 +4,6 @@
 locale-gen pt_BR.UTF-8 && \
 rm -f /etc/localtime && sudo ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
-# Clone the REPO
-# cd /home/ubuntu && sudo -u ubuntu git clone https://github.com/alessanderviana/kubernetes-cluster-in-the-cloud.git
-
 # Ansible
 apt-get update -q
 apt-get install -y software-properties-common
@@ -22,7 +19,7 @@ ansible-playbook /home/ubuntu/kubernetes-cluster-in-the-cloud/ansible/node-insta
 # If it's the node 1, Initialize the cluster
 if [[ "$HOSTNAME" == *"node-1"* ]]; then
   echo -e "[kube-master]\n127.0.0.1 ansible_connection=local\n" | sudo tee -a /etc/ansible/hosts
-  ansible-playbook kube-master /home/ubuntu/kubernetes-cluster-in-the-cloud/ansible/kube-setup-cluster.yml
+  ansible-playbook /home/ubuntu/kubernetes-cluster-in-the-cloud/ansible/kube-setup-cluster.yml
 fi
 
 # Activate the service account
@@ -33,5 +30,3 @@ gcloud auth activate-service-account \
 # Get the kubernetes nodes
 NODE_IPS=$( gcloud compute instances list --filter="(name~kube-cluster-node-[2-9] AND zone:us-central1-b)" --format="value(name,networkInterfaces[0].networkIP)" | awk '{ print $2 }' )
 echo -e "[kube-nodes]\n$NODE_IPS\n" | sudo tee -a /etc/ansible/hosts
-
-# gcloud compute instances list --filter="(name~kube-cluster-node-[2-9] AND zone:us-central1-b)" --format="value(name,networkInterfaces[0].networkIP)"
