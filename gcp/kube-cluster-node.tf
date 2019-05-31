4261c7c6bad9abfa0d94335e433b5c742dfc7782
alessanderviana <alessander.viana@hotmail.com>
2019-05-31 12:35:23 -0300

    Changed to sudo in the remote-exec lines

---
+++ gcp/kube-cluster-node.tf
@@ -0,0 +1,45 @@
+
+resource "google_compute_instance" "kube-cluster-node" {
+ count = 2
+ name         = "kube-cluster-node-${count.index + 1}"
+ machine_type = "n1-standard-2"  # 7.5 GB RAM
+ # machine_type = "n1-standard-1"  # 3.75 GB RAM
+ zone         = "${var.region}-b"
+ allow_stopping_for_update = true
+
+ tags = [ "kube-cluster-node-${count.index + 1}" ]
+
+ boot_disk {
+   initialize_params {
+     image = "ubuntu-1604-xenial-v20190514"
+     size = 20
+   }
+ }
+
+ network_interface {
+   subnetwork = "default"
+   access_config { }
+ }
+
+ metadata {
+   ssh-keys = "${var.user}:${file("${var.pub_key}")}"
+ }
+
+ provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "${var.user}"
+      private_key = "${file("${var.priv_key}")}"
+      agent = false
+    }
+
+    inline = [
+      <<SCRIPT
+      sudo git clone https://github.com/alessanderviana/kubernetes-cluster-in-the-cloud.git && \
+      sudo bash /home/ubuntu/kubernetes-cluster-in-the-cloud/gcp/startup-script.sh
+SCRIPT
+      ,
+    ]
+ }
+
+}
