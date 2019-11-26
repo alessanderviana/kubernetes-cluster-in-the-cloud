44ffdfd44f88dc1276869de945ef7cf6fccc4107
Alessander Viana <alessander.viana@somoseducacao.com.br>
2019-11-26 15:47:50 -0300

    Changed to get the new instance / node

--- terraform-aws/output.tf
+++ terraform-aws/output.tf
@@ -1,7 +1,7 @@
 output "private_ip_address" {
-  value = aws_instance.k8s-aws.private_ip
+  value = [ "${aws_instance.k8s-aws.*.private_ip}" ]
 }
 
 output "public_ip_address" {
-  value = aws_instance.k8s-aws.public_ip
+  value = [ "${aws_instance.k8s-aws.*.public_ip}" ]
 }
