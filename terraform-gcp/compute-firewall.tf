1f4c78feed4b1b5e359ce4fd45cb469ba08e3599
Alessander Viana <alessander.viana@somoseducacao.com.br>
2019-11-28 15:23:55 -0300

    Added new files to work with more terraform files

---
+++ terraform-gcp/compute-firewall.tf
@@ -0,0 +1,27 @@
+resource "google_compute_firewall" "ssh-k8s-aws" {
+  name        = "ssh-k8s-aws"
+  description = "Compute Firewall K8s AWS"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  # target_tags   = "default"
+  source_ranges = ["10.63.116.0/23","10.63.114.0/23","10.63.164.0/24","10.63.14.0/24","185.125.225.22/32"]
+}
+
+resource "google_compute_firewall" "internal-k8s-aws" {
+  name        = "internal-k8s-aws"
+  description = "Compute Firewall K8s AWS"
+  network = "default"
+
+  allow {
+    protocol      = "tcp"
+    ports         = ["0-65535"]
+  }
+
+  # target_tags   = "default"
+  source_ranges = ["10.128.0.0/20"]
+}
