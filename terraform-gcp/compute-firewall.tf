812d446f3714bcb0565c8b48561182798f7590fe
Alessander Viana <alessander.viana@somoseducacao.com.br>
2020-01-10 11:13:11 -0300

    Added ports 80 and 443

--- terraform-gcp/compute-firewall.tf
+++ terraform-gcp/compute-firewall.tf
@@ -25,3 +25,17 @@ resource "google_compute_firewall" "internal-k8s-aws" {
   # target_tags   = "default"
   source_ranges = ["10.128.0.0/20"]
 }
+
+resource "google_compute_firewall" "kube-k8s-aws" {
+  name        = "kube-k8s-aws"
+  description = "Compute Firewall K8s Kubernetes services"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["6443","80","443"]
+  }
+
+  # target_tags   = "default"
+  source_ranges = ["10.63.116.0/23","10.63.114.0/23","10.63.164.0/24","10.63.14.0/24","185.125.225.22/32"]
+}
